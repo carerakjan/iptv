0b56a42cb11a91357cf8a49cdc3b1a5af7af2c0f
Bohdan Hryhorian <carerakjan@gmail.com>
2017-01-29 22:55:37 +0200

    added parse snippet

---
+++ parseM3u.js
@@ -0,0 +1,557 @@
+var playlist = `#EXTM3U
+#EXTINF:0,1+1
+http://tv.iptv.com/stream/original/plus
+#EXTINF:0,112
+http://tv.iptv.com/stream/original/112
+#EXTINF:0,112 HD
+http://tv.iptv.com/stream/original/112-hd
+#EXTINF:0,2+2
+http://tv.iptv.com/stream/original/2-2
+#EXTINF:0,24 Беларусь
+http://tv.iptv.com/stream/original/24-belarus
+#EXTINF:0,24 Док
+http://tv.iptv.com/stream/original/24-dok
+#EXTINF:0,2x2
+http://tv.iptv.com/stream/original/2x2
+#EXTINF:0,5 канал
+http://tv.iptv.com/stream/original/5-kanal
+#EXTINF:0,8 канал
+http://tv.iptv.com/stream/original/8-kanal
+#EXTINF:0,9 Орбита
+http://tv.iptv.com/stream/original/9-orbita
+#EXTINF:0,A-One Hip-Hop
+http://tv.iptv.com/stream/original/a-one-hip-hop
+#EXTINF:0,A-One Ukraine
+http://tv.iptv.com/stream/original/a-one-ukraine
+#EXTINF:0,Amedia 1
+http://tv.iptv.com/stream/original/amedia-1
+#EXTINF:0,Amedia 2
+http://tv.iptv.com/stream/original/amedia-2
+#EXTINF:0,Amedia HD
+http://tv.iptv.com/stream/original/amedia-hd
+#EXTINF:0,Amedia SD
+http://tv.iptv.com/stream/original/amedia-sd
+#EXTINF:0,Animal Planet
+http://tv.iptv.com/stream/original/animalplanet
+#EXTINF:0,Animal Planet HD
+http://tv.iptv.com/stream/original/animal-planet-hd
+#EXTINF:0,ATR
+http://tv.iptv.com/stream/original/atr
+#EXTINF:0,BBC News
+http://tv.iptv.com/stream/original/bbc-news
+#EXTINF:0,Bloomberg
+http://tv.iptv.com/stream/original/bloomberg
+#EXTINF:0,BOUTIQUE TV
+http://tv.iptv.com/stream/original/boutique-tv
+#EXTINF:0,Bridge TV
+http://tv.iptv.com/stream/original/bridge-tv
+#EXTINF:0,Business
+http://tv.iptv.com/stream/original/business
+#EXTINF:0,Cartoon Network
+http://tv.iptv.com/stream/original/cartoon-network
+#EXTINF:0,CBS Drama
+http://tv.iptv.com/stream/original/cbs-drama
+#EXTINF:0,CBS Reality
+http://tv.iptv.com/stream/original/cbs-reality
+#EXTINF:0,Comedy TV
+http://tv.iptv.com/stream/original/comedy-tv
+#EXTINF:0,Da Vinci Learning
+http://tv.iptv.com/stream/original/da-vinci-learning
+#EXTINF:0,Discovery Channel
+http://tv.iptv.com/stream/original/discovery
+#EXTINF:0,Discovery HD
+http://tv.iptv.com/stream/original/discovery-hd
+#EXTINF:0,Discovery Science
+http://tv.iptv.com/stream/original/discovery-science
+#EXTINF:0,Discovery World	
+http://tv.iptv.com/stream/original/discovery-world
+#EXTINF:0,Disney
+http://tv.iptv.com/stream/original/disney
+#EXTINF:0,Diva Universal
+http://tv.iptv.com/stream/original/diva-universal
+#EXTINF:0,E! Russia
+http://tv.iptv.com/stream/original/e-russia
+#EXTINF:0,English Club TV
+http://tv.iptv.com/stream/original/english-club-tv
+#EXTINF:0,Enter Film
+http://tv.iptv.com/stream/original/enter-film
+#EXTINF:0,Euronews
+http://tv.iptv.com/stream/original/euronews
+#EXTINF:0,Euronews UA
+http://tv.iptv.com/stream/original/euronews-ua
+#EXTINF:0,Eurosport
+http://tv.iptv.com/stream/original/eurosport
+#EXTINF:0,Eurosport 2
+http://tv.iptv.com/stream/original/eurosport-2
+#EXTINF:0,Eurosport HD
+http://tv.iptv.com/stream/original/eurosport-hd
+#EXTINF:0,Extreme Sports
+http://tv.iptv.com/stream/original/extreme-sports
+#EXTINF:0,Fashion TV Russia
+http://tv.iptv.com/stream/original/fashion-tv-russia
+#EXTINF:0,FOX
+http://tv.iptv.com/stream/original/fox
+#EXTINF:0,FOX Life
+http://tv.iptv.com/stream/original/fox-life
+#EXTINF:0,France 24
+http://tv.iptv.com/stream/original/france-24
+#EXTINF:0,Gamma
+http://tv.iptv.com/stream/original/gamma
+#EXTINF:0,Gulli
+http://tv.iptv.com/stream/original/gulli
+#EXTINF:0,HD Fashion
+http://tv.iptv.com/stream/original/hd-fashion
+#EXTINF:0,HD Life
+http://tv.iptv.com/stream/original/hd-life
+#EXTINF:0,HD Кино
+http://tv.iptv.com/stream/original/hd-kino
+#EXTINF:0,HD Кино 2
+http://tv.iptv.com/stream/original/hd-kino-2
+#EXTINF:0,HD Спорт
+http://tv.iptv.com/stream/original/hd-sport
+#EXTINF:0,History HD
+http://tv.iptv.com/stream/original/history-hd
+#EXTINF:0,History SD
+http://tv.iptv.com/stream/original/history-sd
+#EXTINF:0,ICTV
+http://tv.iptv.com/stream/original/ictv
+#EXTINF:0,Impact TV
+http://tv.iptv.com/stream/original/impact-tv
+#EXTINF:0,Investigation Discovery
+http://tv.iptv.com/stream/original/investigation_discovery
+#EXTINF:0,JimJam
+http://tv.iptv.com/stream/original/jimjam
+#EXTINF:0,K2
+http://tv.iptv.com/stream/original/k2
+#EXTINF:0,Kino News
+http://tv.iptv.com/stream/original/kino-news
+#EXTINF:0,MCM Top
+http://tv.iptv.com/stream/original/mcm-top
+#EXTINF:0,Menu TV
+http://tv.iptv.com/stream/original/menu-tv
+#EXTINF:0,Mezzo HD
+http://tv.iptv.com/stream/original/mezzo-hd
+#EXTINF:0,MGM channel
+http://tv.iptv.com/stream/original/mgm-channel
+#EXTINF:0,MGM channel HD
+http://tv.iptv.com/stream/original/mgm-channel-hd
+#EXTINF:0,MTV
+http://tv.iptv.com/stream/original/mtv
+#EXTINF:0,MTV Dance
+http://tv.iptv.com/stream/original/mtv-dance
+#EXTINF:0,MTV Hits
+http://tv.iptv.com/stream/original/mtv-hits
+#EXTINF:0,MTV Live HD
+http://tv.iptv.com/stream/original/mtv-live-hd
+#EXTINF:0,MTV Rocks
+http://tv.iptv.com/stream/original/mtv-rocks
+#EXTINF:0,Music BOX
+http://tv.iptv.com/stream/original/music-box
+#EXTINF:0,Nat Geo HD
+http://tv.iptv.com/stream/original/nat-geo-hd
+#EXTINF:0,Nat Geo Wild
+http://tv.iptv.com/stream/original/nat-geo-wild
+#EXTINF:0,Nat Geo Wild HD
+http://tv.iptv.com/stream/original/nat-geo-wild-hd
+#EXTINF:0,National Geographic
+http://tv.iptv.com/stream/original/natgeo
+#EXTINF:0,News One
+http://tv.iptv.com/stream/original/news-one
+#EXTINF:0,NHK World
+http://tv.iptv.com/stream/original/nhk-world
+#EXTINF:0,Nickelodeon
+http://tv.iptv.com/stream/original/nickelodeon
+#EXTINF:0,Nickelodeon HD
+http://tv.iptv.com/stream/original/nickelodeon-hd
+#EXTINF:0,Ocean TV
+http://tv.iptv.com/stream/original/ocean-tv
+#EXTINF:0,OE
+http://tv.iptv.com/stream/original/oe
+#EXTINF:0,OTV
+http://tv.iptv.com/stream/original/otv
+#EXTINF:0,Outdoor Channel
+http://tv.iptv.com/stream/original/outdoor-channel
+#EXTINF:0,Paramount Comedy
+http://tv.iptv.com/stream/original/paramount-comedy
+#EXTINF:0,PRO ВСЕ
+http://tv.iptv.com/stream/original/pro-vse
+#EXTINF:0,QTV
+http://tv.iptv.com/stream/original/qtv
+#EXTINF:0,Real Estate TV
+http://tv.iptv.com/stream/original/real-estate-tv
+#EXTINF:0,RTG TV
+http://tv.iptv.com/stream/original/rtg-tv
+#EXTINF:0,RTVi
+http://tv.iptv.com/stream/original/rtvi
+#EXTINF:0,RU Music
+http://tv.iptv.com/stream/original/ru-music
+#EXTINF:0,RU.TV
+http://tv.iptv.com/stream/original/ru-tv
+#EXTINF:0,Rusong TV
+http://tv.iptv.com/stream/original/rusong-tv
+#EXTINF:0,Russian Extreme
+http://tv.iptv.com/stream/original/russian-extreme
+#EXTINF:0,S TV
+http://tv.iptv.com/stream/original/s-tv
+#EXTINF:0,Shopping TV
+http://tv.iptv.com/stream/original/shopping-tv
+#EXTINF:0,TiJi
+http://tv.iptv.com/stream/original/tiji
+#EXTINF:0,TLC
+http://tv.iptv.com/stream/original/tlc
+#EXTINF:0,TLC HD
+http://tv.iptv.com/stream/original/tlc-hd
+#EXTINF:0,Travel Channel
+http://tv.iptv.com/stream/original/travel-channel
+#EXTINF:0,Travel Channel HD
+http://tv.iptv.com/stream/original/travel-channel-hd
+#EXTINF:0,TV 1000
+http://tv.iptv.com/stream/original/tv-1000
+#EXTINF:0,TV 1000 Action
+http://tv.iptv.com/stream/original/tv-1000-action
+#EXTINF:0,TV 1000 Русское кино
+http://tv.iptv.com/stream/original/tv-1000-russkoe-kino
+#EXTINF:0,TV XXI
+http://tv.iptv.com/stream/original/tv-xxi
+#EXTINF:0,TV1000 Comedy HD
+http://tv.iptv.com/stream/original/tv1000-comedy-hd
+#EXTINF:0,TV1000 MegaHit HD
+http://tv.iptv.com/stream/original/tv1000-megahit-hd
+#EXTINF:0,TV1000 Premium HD
+http://tv.iptv.com/stream/original/tv1000-premium-hd
+#EXTINF:0,TV5 Monde
+http://tv.iptv.com/stream/original/tv5-monde
+#EXTINF:0,UBR
+http://tv.iptv.com/stream/original/ubr
+#EXTINF:0,Ukrainian fashion
+http://tv.iptv.com/stream/original/ukrainian-fashion
+#EXTINF:0,Universal
+http://tv.iptv.com/stream/original/universal
+#EXTINF:0,VH1 Classic
+http://tv.iptv.com/stream/original/vh1-classic
+#EXTINF:0,VH1 Music
+http://tv.iptv.com/stream/original/vh1-music
+#EXTINF:0,Viasat Explorer
+http://tv.iptv.com/stream/original/viasat-explorer
+#EXTINF:0,Viasat History
+http://tv.iptv.com/stream/original/viasat-history
+#EXTINF:0,Viasat Nature
+http://tv.iptv.com/stream/original/viasat-nature
+#EXTINF:0,Viasat Nature HD
+http://tv.iptv.com/stream/original/viasat-nature-hd
+#EXTINF:0,Viasat Sport
+http://tv.iptv.com/stream/original/viasat-sport
+#EXTINF:0,Viasat Sport East HD
+http://tv.iptv.com/stream/original/viasat-sport-east-hd
+#EXTINF:0,World Fashion
+http://tv.iptv.com/stream/original/world-fashion
+#EXTINF:0,Zik
+http://tv.iptv.com/stream/original/zik
+#EXTINF:0,Zoom
+http://tv.iptv.com/stream/original/zoom
+#EXTINF:0,Авто Плюс
+http://tv.iptv.com/stream/original/avto-plyus
+#EXTINF:0,Боец ТВ
+http://tv.iptv.com/stream/original/boets-tv
+#EXTINF:0,Бойцовский клуб
+http://tv.iptv.com/stream/original/boytsovskiy-klub
+#EXTINF:0,БТБ
+http://tv.iptv.com/stream/original/btb
+#EXTINF:0,Возрождение
+http://tv.iptv.com/stream/original/vozrozhdenie
+#EXTINF:0,Вопросы и ответы
+http://tv.iptv.com/stream/original/voprosy-i-otvety
+#EXTINF:0,Время: далекое и близкое
+http://tv.iptv.com/stream/original/vremya
+#EXTINF:0,Глас
+http://tv.iptv.com/stream/original/glas
+#EXTINF:0,Громадське ТБ
+http://tv.iptv.com/stream/original/gromadske-tb
+#EXTINF:0,Детский
+http://tv.iptv.com/stream/original/detskiy
+#EXTINF:0,Детский мир
+http://tv.iptv.com/stream/original/detskiy-mir
+#EXTINF:0,Добро ТВ
+http://tv.iptv.com/stream/original/dobro-tv
+#EXTINF:0,Дождь
+http://tv.iptv.com/stream/original/dozhd
+#EXTINF:0,Дом кино
+http://tv.iptv.com/stream/original/dom-kino
+#EXTINF:0,Домашние животные
+http://tv.iptv.com/stream/original/domashnie-zhivotnye
+#EXTINF:0,Домашний
+http://tv.iptv.com/stream/original/domashniy
+#EXTINF:0,Драйв
+http://tv.iptv.com/stream/original/drayv
+#EXTINF:0,Еврокино
+http://tv.iptv.com/stream/original/evrokino
+#EXTINF:0,Европа Плюс ТВ
+http://tv.iptv.com/stream/original/evropa-plyus-tv
+#EXTINF:0,Еко ТВ
+http://tv.iptv.com/stream/original/eko-tv
+#EXTINF:0,Еспресо ТВ
+http://tv.iptv.com/stream/original/espreso-tv
+#EXTINF:0,Живая Планета
+http://tv.iptv.com/stream/original/zhivaya-planeta
+#EXTINF:0,Загородный
+http://tv.iptv.com/stream/original/zagorodnyy
+#EXTINF:0,Здоровое ТВ
+http://tv.iptv.com/stream/original/zdorovoe-tv
+#EXTINF:0,Иллюзион+
+http://tv.iptv.com/stream/original/illyuzion
+#EXTINF:0,Индия ТВ
+http://tv.iptv.com/stream/original/indiya-tv
+#EXTINF:0,Интер
+http://tv.iptv.com/stream/original/inter
+#EXTINF:0,Интересное ТВ
+http://tv.iptv.com/stream/original/interesnoe-tv
+#EXTINF:0,К1
+http://tv.iptv.com/stream/original/k1
+#EXTINF:0,Канал 2
+http://tv.iptv.com/stream/original/kanal-2
+#EXTINF:0,Карусель
+http://tv.iptv.com/stream/original/karusel
+#EXTINF:0,КДРТРК
+http://tv.iptv.com/stream/original/kdrtrk
+#EXTINF:0,Кино плюс
+http://tv.iptv.com/stream/original/kino-plyus
+#EXTINF:0,Кино Союз
+http://tv.iptv.com/stream/original/kino-soyuz
+#EXTINF:0,Киноклуб
+http://tv.iptv.com/stream/original/kinoklub
+#EXTINF:0,Кинопоказ ТВ
+http://tv.iptv.com/stream/original/kinopokaz-tv
+#EXTINF:0,Кинохит
+http://tv.iptv.com/stream/original/kinohit
+#EXTINF:0,Комедия ТВ
+http://tv.iptv.com/stream/original/komediya-tv
+#EXTINF:0,КРТ
+http://tv.iptv.com/stream/original/krt
+#EXTINF:0,Кто есть Кто
+http://tv.iptv.com/stream/original/kto-est-kto
+#EXTINF:0,Культура
+http://tv.iptv.com/stream/original/kultura
+#EXTINF:0,Кухня ТВ
+http://tv.iptv.com/stream/original/kuhnya-tv
+#EXTINF:0,КХЛ
+http://tv.iptv.com/stream/original/khl
+#EXTINF:0,Львів ТБ
+http://tv.iptv.com/stream/original/lviv-tb
+#EXTINF:0,Ля Минор
+http://tv.iptv.com/stream/original/lya-minor
+#EXTINF:0,М1
+http://tv.iptv.com/stream/original/m1
+#EXTINF:0,М2
+http://tv.iptv.com/stream/original/m2
+#EXTINF:0,МАКСІ ТВ
+http://tv.iptv.com/stream/original/maksi-tv
+#EXTINF:0,Малятко
+http://tv.iptv.com/stream/original/malyatko
+#EXTINF:0,Мама
+http://tv.iptv.com/stream/original/mama
+#EXTINF:0,Мега
+http://tv.iptv.com/stream/original/mega
+#EXTINF:0,Мир 24
+http://tv.iptv.com/stream/original/mir-24
+#EXTINF:0,Много ТВ
+http://tv.iptv.com/stream/original/mnogo-tv
+#EXTINF:0,Москва 24
+http://tv.iptv.com/stream/original/moskva-24
+#EXTINF:0,Моя Дитина
+http://tv.iptv.com/stream/original/moya-ditina
+#EXTINF:0,Моя планета
+http://tv.iptv.com/stream/original/moya-planeta
+#EXTINF:0,Мужской
+http://tv.iptv.com/stream/original/muzhskoy
+#EXTINF:0,МУЗ ТВ
+http://tv.iptv.com/stream/original/muz-tv
+#EXTINF:0,Музыка первого
+http://tv.iptv.com/stream/original/muzyka-pervogo
+#EXTINF:0,Мультимания
+http://tv.iptv.com/stream/original/multimaniya
+#EXTINF:0,Надія
+http://tv.iptv.com/stream/original/nadiya
+#EXTINF:0,Наука 2.0
+http://tv.iptv.com/stream/original/nauka-2-0
+#EXTINF:0,Наш Футбол
+http://tv.iptv.com/stream/original/ntv-nash-futbol
+#EXTINF:0,Наш Футбол HD
+http://tv.iptv.com/stream/original/nash-futbol-hd
+#EXTINF:0,Наше кино
+http://tv.iptv.com/stream/original/nashe_kino
+#EXTINF:0,Наше любимое кино
+http://tv.iptv.com/stream/original/nashe-lyubimoe-kino
+#EXTINF:0,Наше Новое Кино
+http://tv.iptv.com/stream/original/nashe-novoe-kino
+#EXTINF:0,НЛО ТВ
+http://tv.iptv.com/stream/original/nlo-tv
+#EXTINF:0,Новий канал
+http://tv.iptv.com/stream/original/novykanal
+#EXTINF:0,Новини 24
+http://tv.iptv.com/stream/original/novini-24
+#EXTINF:0,Ностальгия
+http://tv.iptv.com/stream/original/nostalgiya
+#EXTINF:0,НСТ
+http://tv.iptv.com/stream/original/nst
+#EXTINF:0,НТВ+ 3D by Panasonic
+http://tv.iptv.com/stream/original/3d-by-panasonic
+#EXTINF:0,НТВ+ Баскетбол
+http://tv.iptv.com/stream/original/ntv-basketbol
+#EXTINF:0,НТВ+ Спорт
+http://tv.iptv.com/stream/original/ntv_plus_sport
+#EXTINF:0,НТВ+ Спорт Плюс
+http://tv.iptv.com/stream/original/ntv-sport-plyus
+#EXTINF:0,НТВ+ Спорт Союз
+http://tv.iptv.com/stream/original/ntv-sport-soyuz
+#EXTINF:0,НТВ+ Теннис
+http://tv.iptv.com/stream/original/ntv-tennis
+#EXTINF:0,НТВ+ Футбол 1
+http://tv.iptv.com/stream/original/ntvfutbol
+#EXTINF:0,НТВ+ Футбол 2
+http://tv.iptv.com/stream/original/ntv-futbol-2
+#EXTINF:0,НТВ+ Футбол 2 HD
+http://tv.iptv.com/stream/original/ntv-futbol-2-hd
+#EXTINF:0,НТВ+ Футбол 3 HD
+http://tv.iptv.com/stream/original/ntv-futbol-3-hd
+#EXTINF:0,НТВ+ Футбол HD
+http://tv.iptv.com/stream/original/ntv-futbol-hd
+#EXTINF:0,НТН
+http://tv.iptv.com/stream/original/ntn
+#EXTINF:0,Оружие
+http://tv.iptv.com/stream/original/oruzhie
+#EXTINF:0,ОТР
+http://tv.iptv.com/stream/original/otr
+#EXTINF:0,Охота и рыбалка
+http://tv.iptv.com/stream/original/ohota-i-rybalka
+#EXTINF:0,Парк Развлечений
+http://tv.iptv.com/stream/original/park-razvlecheniy
+#EXTINF:0,Перец ТВ
+http://tv.iptv.com/stream/original/perets-tv
+#EXTINF:0,Перший
+http://tv.iptv.com/stream/original/pershiy
+#EXTINF:0,Перший діловий
+http://tv.iptv.com/stream/original/pershiy-diloviy
+#EXTINF:0,Перший Національний
+http://tv.iptv.com/stream/original/pershiy-natsionalniy
+#EXTINF:0,Пиксель
+http://tv.iptv.com/stream/original/piksel
+#EXTINF:0,Планета ТВ
+http://tv.iptv.com/stream/original/planeta-tv
+#EXTINF:0,Плюс Плюс
+http://tv.iptv.com/stream/original/plyus-plyus
+#EXTINF:0,Право TV
+http://tv.iptv.com/stream/original/pravo-tv
+#EXTINF:0,Премьера
+http://tv.iptv.com/stream/original/premiera
+#EXTINF:0,Просвещение
+http://tv.iptv.com/stream/original/prosveschenie
+#EXTINF:0,Психология21
+http://tv.iptv.com/stream/original/psihologiya21
+#EXTINF:0,Пятница
+http://tv.iptv.com/stream/original/pyatnitsa
+#EXTINF:0,Рада
+http://tv.iptv.com/stream/original/rada
+#EXTINF:0,Ретро
+http://tv.iptv.com/stream/original/retro
+#EXTINF:0,Россия 2
+http://tv.iptv.com/stream/original/rossiya-2
+#EXTINF:0,Россия K
+http://tv.iptv.com/stream/original/rosk
+#EXTINF:0,Русский иллюзион
+http://tv.iptv.com/stream/original/russkiy-illyuzion
+#EXTINF:0,Совершенно Секретно
+http://tv.iptv.com/stream/original/sovershenno-sekretno
+#EXTINF:0,Сонце
+http://tv.iptv.com/stream/original/sontse
+#EXTINF:0,Спас
+http://tv.iptv.com/stream/original/spas
+#EXTINF:0,Спорт
+http://tv.iptv.com/stream/original/sport
+#EXTINF:0,Спорт 1
+http://tv.iptv.com/stream/original/sport-1
+#EXTINF:0,Спорт 1 HD
+http://tv.iptv.com/stream/original/sport-1-hd
+#EXTINF:0,Спорт 1 Украина
+http://tv.iptv.com/stream/original/sport-1-ukraina
+#EXTINF:0,Спорт 2 Украина
+http://tv.iptv.com/stream/original/sport-2-ukraina
+#EXTINF:0,Спортхит
+http://tv.iptv.com/stream/original/sporthit
+#EXTINF:0,СТБ
+http://tv.iptv.com/stream/original/stb
+#EXTINF:0,СТС
+http://tv.iptv.com/stream/original/sts
+#EXTINF:0,ТВ3
+http://tv.iptv.com/stream/original/tv3
+#EXTINF:0,ТВi
+http://tv.iptv.com/stream/original/tvi
+#EXTINF:0,ТВЦ
+http://tv.iptv.com/stream/original/tvts
+#EXTINF:0,ТДК
+http://tv.iptv.com/stream/original/tdk
+#EXTINF:0,Телеканал 100
+http://tv.iptv.com/stream/original/telekanal-100
+#EXTINF:0,Телеканал Футбол
+http://tv.iptv.com/stream/original/telekanal-futbol
+#EXTINF:0,Телекафе
+http://tv.iptv.com/stream/original/telekafe
+#EXTINF:0,ТЕТ
+http://tv.iptv.com/stream/original/tet
+#EXTINF:0,Техно 24
+http://tv.iptv.com/stream/original/24-tehno
+#EXTINF:0,ТНВ Планета
+http://tv.iptv.com/stream/original/tnv-planeta
+#EXTINF:0,Тонис HD
+http://tv.iptv.com/stream/original/tonis
+#EXTINF:0,ТРК Київ
+http://tv.iptv.com/stream/original/trk-kiyiv
+#EXTINF:0,ТРК Україна
+http://tv.iptv.com/stream/original/trk-ukraina
+#EXTINF:0,Трофей
+http://tv.iptv.com/stream/original/trofey
+#EXTINF:0,Уніан
+http://tv.iptv.com/stream/original/unian
+#EXTINF:0,УНТ
+http://tv.iptv.com/stream/original/unt
+#EXTINF:0,Усадьба
+http://tv.iptv.com/stream/original/usadba
+#EXTINF:0,Успех
+http://tv.iptv.com/stream/original/uspeh
+#EXTINF:0,Феникс + кино
+http://tv.iptv.com/stream/original/feniks-kino
+#EXTINF:0,Футбол 1
+http://tv.iptv.com/stream/original/futbol1
+#EXTINF:0,Футбол 2
+http://tv.iptv.com/stream/original/futbol2
+#EXTINF:0,Хокей
+http://tv.iptv.com/stream/original/hokey
+#EXTINF:0,Черноморска ТВ
+http://tv.iptv.com/stream/original/chernomorska-tv
+#EXTINF:0,ЧП Інфо
+http://tv.iptv.com/stream/original/chp-info
+#EXTINF:0,Шансон ТВ
+http://tv.iptv.com/stream/original/shanson-tv
+#EXTINF:0,Ю ТВ
+http://tv.iptv.com/stream/original/yu-tv
+`;
+
+function convertInto2KOM(m3u) {
+    return m3u
+        .replace('#EXTM3U', '')
+        .split('#EXTINF:0,')
+        .slice(1)
+        .map(function(str, index) {
+            var channel = str.split('\n').slice(0,-1);
+
+            return {
+                "id": index + 1,
+                "number": index + 1,
+                "caption": channel[0],
+                "icon_url": "",
+                "tv_categories": [2],
+                "streaming_url": channel[1],
+                "announce": "",
+                "volume_shift": 0
+            };
+        });
+}
